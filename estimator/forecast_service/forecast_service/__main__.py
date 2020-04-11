c2b5a7b96b3f0ee35968efc5967f5f24a60a6fca
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-04-11 14:51:14 +0200

    Parse config argument

--- estimator/forecast_service/forecast_service/__main__.py
+++ estimator/forecast_service/forecast_service/__main__.py
@@ -1,9 +1,14 @@
-from forecast_service.handler import Handler
-from forecast_service.estimator import RnnEstimator
+import argparse
+import configparser
 
 def main():
-  handler = 
+  # Arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--config', type=str, default='/etc/forecast/forecast.yml', help='configuration file for the forecasting service')  
+  args = vars(parser.parse_args())
 
+  # Read and parse config file
+  
 
 if __name__ == '__main__':
   main()
