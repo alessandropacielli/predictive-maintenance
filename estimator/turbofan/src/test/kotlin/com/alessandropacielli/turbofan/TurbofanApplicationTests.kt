40003ae3925d45446b1adbd66fa73f1b164e376e
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-04-16 21:18:13 +0200

    Init spring project

---
+++ estimator/turbofan/src/test/kotlin/com/alessandropacielli/turbofan/TurbofanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alessandropacielli.turbofan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TurbofanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
