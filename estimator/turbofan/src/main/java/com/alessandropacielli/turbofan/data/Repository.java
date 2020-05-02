84edbeadabddbb965f33df3cf06e3864d34ee34a
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-05-02 15:05:02 +0200

    Add ndarray return type for getLast method in repo

--- estimator/turbofan/src/main/java/com/alessandropacielli/turbofan/data/Repository.java
+++ estimator/turbofan/src/main/java/com/alessandropacielli/turbofan/data/Repository.java
@@ -1,13 +1,25 @@
 package com.alessandropacielli.turbofan.data;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+
 import java.util.List;
 
 public interface Repository<Model> {
     /**
      * Returns the last n measurements for the required device
-     *  @param device : String, identifies the device
-     * @param n :      Int, limits the query to the needed amount of data points
-     * @return
+     * @param device : identifies the device for which we'd like to obtain the measurements
+     * @param n : Limits the query to the needed amount of data points
+     * @return a list of the last n measurements for the device
      */
-    public List<Model> getLastMeasurements(String device, int n);
+    List<Model> getLastMeasurements(String device, int n);
+
+    /**
+     * Returns only the double fields of the last n measurements for the required device in the form of
+     * @param device identifies the device for which we'd like to identify the measurements
+     * @param n Limits the query to the needed amount of data points
+     * @param doubleFields The list of fields
+     * @return a 2D array of the last n measurements for the device
+     */
+    INDArray getLastMeasurements(String device, int n, List<String> doubleFields);
+
 }
--- estimator/turbofan/src/main/java/com/alessandropacielli/turbofan/data/influxdb/InfluxDBRepository.java
+++ estimator/turbofan/src/main/java/com/alessandropacielli/turbofan/data/influxdb/InfluxDBRepository.java
@@ -2,13 +2,22 @@ package com.alessandropacielli.turbofan.data.influxdb;
 
 import com.alessandropacielli.turbofan.data.Repository;
 import com.alessandropacielli.turbofan.models.TurbofanModel;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.schema.Schema;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
 import org.influxdb.impl.InfluxDBMapper;
+import org.influxdb.querybuilder.Column;
 import org.jetbrains.annotations.NotNull;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.influxdb.querybuilder.BuiltQuery.QueryBuilder.*;
 
@@ -40,7 +49,54 @@ public class InfluxDBRepository<Model> implements Repository<Model> {
         return mapper.query(query, clazz);
     }
 
-    public static void main(String args[]) {
+    @Override
+    public INDArray getLastMeasurements(String device, int n, List<String> doubleFields) {
+        Map<String, Integer> indexes = new HashMap<>();
+        for(int i = 0; i < doubleFields.size(); i++) {
+            indexes.put(doubleFields.get(i), i);
+        }
+
+        Query query = select()
+                .from(database, measurement)
+                .orderBy(desc())
+                .where(eq("device", device))
+                .limit(n);
+        QueryResult queryResult = this.client.query(query);
+
+        // TODO handle queryResult.getErrors
+
+        // The query should have only one result
+        List<QueryResult.Result> queryResultList = queryResult.getResults();
+        if(queryResultList.size() == 1) {
+            List<QueryResult.Series> series = queryResultList.get(0).getSeries();
+            if(series.size() == 1) {
+                QueryResult.Series s = series.get(0);
+                List<String> columns = s.getColumns();
+                List<List<Object>> values = s.getValues();
+
+                INDArray result = Nd4j.zeros(n, columns.size());
+
+                for (int record = 0; record < n; record++) {
+                    for (int column = 0; column < columns.size(); column++) {
+                        if (doubleFields.contains(columns.get(column))) {
+                            result.put(record, indexes.get(columns.get(column)), (double)values.get(record).get(column));
+                        }
+                    }
+                }
+
+                return result;
+
+            } else {
+                // TODO error
+                return null;
+            }
+        } else {
+            // TODO error
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
         InfluxDB client = InfluxDBFactory.connect("http://localhost:8086");
         InfluxDBRepository<TurbofanModel> repo = new InfluxDBRepository<>(client, "test", "normalized", TurbofanModel.class);
         System.out.println(repo.getLastMeasurements("test", 10));
