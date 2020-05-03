a8ccad8c6c911736c873a5fd6a5ad6924655ba10
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-05-03 16:31:08 +0200

    Remove abstract classes

--- estimator/forecast_service/forecast_service/estimator.py
+++ estimator/forecast_service/forecast_service/estimator.py
@@ -1,15 +1,9 @@
-from abc import ABC, abstractmethod
-
-class Estimator(ABC):
-
-  @abstractmethod
-  def predict(self, input):
-    pass
+from keras.models import load_model
 
 class RnnEstimator():
 
   def __init__(self, model_path):
-    from keras.models import load_model
+
     super().__init__()
     self.model = load_model(model_path)
 
