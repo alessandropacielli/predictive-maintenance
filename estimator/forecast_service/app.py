7d492c1f41e814d9ff5cbaf56e1e26f44418620a
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-05-07 12:29:23 +0200

    Remove measurement from route

--- estimator/forecast_service/app.py
+++ estimator/forecast_service/app.py
@@ -3,10 +3,11 @@ from forecast_service.estimator import RnnEstimator
 from forecast_service.normalization import PickledScaler
 from forecast_service.db import InfluxDB
 import numpy as np
+import time
 
 app = Flask(__name__)
 
-db = InfluxDB('localhost', 8086, 'predictive-maintenance')
+db = InfluxDB('localhost', 8086, 'predictive-maintenance', 'turbofan')
 estimator = RnnEstimator('./resources/models/rul_regression_v1.h5')
 preprocessor = PickledScaler('./resources/preprocessors/turbofan_scaler.pkl')
 
@@ -14,10 +15,10 @@ sensor_cols = ['s' + str(i) for i in range(1,22)]
 sequence_cols = ['setting1', 'setting2', 'setting3', 'cycle']
 sequence_cols.extend(sensor_cols)
 
-@app.route('/<measurement>/<device>', methods=['GET', 'POST'])
-def turbofan(measurement, device):
+@app.route('/turbofan/<device>', methods=['GET', 'POST'])
+def turbofan(device):
   device = int(device)
   sequence_length = estimator.get_sequence_length()
-  data = db.get_last(sequence_length, device, measurement, order=sequence_cols)
+  data = db.get_last(sequence_length, device, order=sequence_cols)
   data_norm = np.array(preprocessor.transform(data))
   return { 'prediction': str(estimator.predict(np.reshape(data_norm, (1, sequence_length, len(sequence_cols))))[0][0]) }
--- estimator/forecast_service/forecast_service/db.py
+++ estimator/forecast_service/forecast_service/db.py
@@ -2,13 +2,14 @@ from influxdb import DataFrameClient
 
 class InfluxDB():
 
-  def __init__(self, host, port, database):
+  def __init__(self, host, port, database, measurement):
     super().__init__()
     self.client = DataFrameClient(host=host, port=port, database=database)
+    self.measurement = measurement
 
-  def get_last(self, n, device, measurement, order=None):
-    query = 'SELECT * FROM ' + str(measurement) + ' WHERE device=$device ORDER BY time DESC LIMIT ' + str(int(n)) 
-    result = self.client.query(query, bind_params={'device': device})[measurement].drop('device', axis=1)
+  def get_last(self, n, device, order=None):
+    query = 'SELECT * FROM ' + str(self.measurement) + ' WHERE device=$device ORDER BY time DESC LIMIT ' + str(int(n)) 
+    result = self.client.query(query, bind_params={'device': device})[self.measurement].drop('device', axis=1)
 
     if order is not None:
       result = result[order]
