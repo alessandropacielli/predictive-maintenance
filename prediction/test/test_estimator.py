393eef8c51170c768c6bf680739238f0bba7c446
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-06-03 19:29:45 +0200

    Comment estimator tests

--- prediction/test/test_estimator.py
+++ prediction/test/test_estimator.py
@@ -3,7 +3,7 @@ from prediction.estimator import RnnEstimator
 from prediction.preprocessing import PickledPreprocessor
 from keras.models import load_model
 import random
-from util import get_sample_test_data
+from util import sample_test_data
 import pandas as pd
 import numpy as np
 
@@ -12,21 +12,32 @@ class RnnEstimatorTest(unittest.TestCase):
 
   def setUp(self):
     self.model_path_v1 = './resources/prediction/rul_regression_v1.h5'
+    self.sequence_length = 50
     self.estimator = RnnEstimator(self.model_path_v1)
     self.preprocessor = PickledPreprocessor('./resources/preprocessing/turbofan_scaler.pkl')
 
   def test_get_sequence_length(self):
-    assert self.estimator.get_sequence_length() == 50
+    """
+    Checks the returned sequence length is right
+    """
+    assert self.estimator.get_sequence_length() == self.sequence_length
 
   def test_predictions_match(self):
+    """
+    Checks the RnnEstimator behaves as it should
+    """
     # Load model from path
     loaded_model = load_model(self.model_path_v1)
     sequence_length = self.estimator.get_sequence_length()
 
     # Obtain sample data
-    random_data = pd.DataFrame(self.preprocessor.transform(get_sample_test_data()))
+    random_data = sample_test_data()
+
+    # Preprocess sample data
+    random_data = pd.DataFrame(self.preprocessor.transform(random_data))
     len_random_data = len(random_data)
 
+    # Obtain sequences from sample data
     random_data_sequences = np.array([random_data[start:stop].values for start, stop in zip(range(0, len_random_data - sequence_length), range(sequence_length, len_random_data))])
 
     # Assert prediction of keras model is the same as the estimator
