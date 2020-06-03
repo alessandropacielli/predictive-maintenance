165f419d7c0dcb2ab7b331e54d3222e4af812462
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-06-03 18:55:45 +0200

    Comment prediction

--- prediction/prediction/estimator.py
+++ prediction/prediction/estimator.py
@@ -2,13 +2,22 @@ from tensorflow.keras.models import load_model
 import numpy as np
 
 class RnnEstimator():
+  """
+  Loads a keras model serialized in hdf5 format
+  """
 
   def __init__(self, model_path: str):
     super().__init__()
     self.model = load_model(model_path)
 
   def get_sequence_length(self):
+    """
+    Returns the number of instances the input needs to produce an output
+    """
     return self.model.input_shape[1]
 
   def predict(self, input: np.ndarray):
+    """
+    Evaluates input and returns a prediction
+    """
     return self.model.predict(input)
