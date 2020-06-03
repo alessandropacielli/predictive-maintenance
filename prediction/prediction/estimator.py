fdd12ef69ef4de4724758f720fe5dae5f7740dee
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-06-03 18:50:43 +0200

    Add type annotations to estimator and preprocessor

--- prediction/prediction/estimator.py
+++ prediction/prediction/estimator.py
@@ -1,13 +1,14 @@
 from tensorflow.keras.models import load_model
+import numpy as np
 
 class RnnEstimator():
 
-  def __init__(self, model_path):
+  def __init__(self, model_path: str):
     super().__init__()
     self.model = load_model(model_path)
 
   def get_sequence_length(self):
     return self.model.input_shape[1]
 
-  def predict(self, input):
+  def predict(self, input: np.ndarray):
     return self.model.predict(input)
--- prediction/prediction/preprocessing.py
+++ prediction/prediction/preprocessing.py
@@ -2,12 +2,18 @@ import joblib
 import pandas as pd
 
 class PickledPreprocessor():
-  def __init__(self, path):
+  """
+  Util class for loading a pickled scikit-learn scaler and transforming pandas dataframes with it
+  """
+  def __init__(self, path: str):
     super().__init__()
     self.scaler = joblib.load(path)
 
 
-  def transform(self, data):
+  def transform(self, data: pd.DataFrame):
+    """
+    Accepts a dataframe and transforms it 
+    """
     normalized_data = pd.DataFrame(
       self.scaler.fit_transform(data), 
       index=data.index
