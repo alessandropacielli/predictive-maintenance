64b39653660f82906f97c71f3e6eeacdab752862
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-06-06 12:09:35 +0200

    Move training

--- prediction/test/util.py
+++ prediction/test/util.py
@@ -3,7 +3,7 @@ import pandas as pd
 import random
 
 def sample_test_data():
-  test_data_path = '../estimator/training/data/PM_test.txt'
+  test_data_path = '../training/data/PM_test.txt'
   columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3', 
     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11','s12', 's13', 's14', 
     's15', 's16', 's17', 's18', 's19','s20', 's21']
